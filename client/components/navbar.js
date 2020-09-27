711641347a63e881bc64f6172cc9f7641211838c
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-27 18:35:37 +0530

    Create navbar.js

---
+++ client/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from 'next/link';
+
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
