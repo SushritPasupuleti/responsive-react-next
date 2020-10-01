ec125d8a355b87ae65216771362ced650a749fa5
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-01 21:11:11 +0530

    Update Button.js

--- client/components/CyberPunkButton/Button.js
+++ client/components/CyberPunkButton/Button.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { ParentButton, ButtonContent } from './Button.elements'
+import { ParentButton, ButtonContent, ButtonLabel } from './Button.elements'
 
 function Button() {
     return (
         <ParentButton>
             <ButtonContent>2077 Text_</ButtonContent>
+            <ButtonLabel>r25</ButtonLabel>
         </ParentButton>
     )
 }
