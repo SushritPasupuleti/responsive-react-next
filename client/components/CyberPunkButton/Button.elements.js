98bdf9d47dfb70401fbcd87ae847b03b478bcf59
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-01 20:56:16 +0530

    Create Button.elements.js

---
+++ client/components/CyberPunkButton/Button.elements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ParentButton = styled.button`
+:root {
+  --yellow-color: #fcee09; 
+  --red-color: #ff003c;
+  --black-color: #050a0e;
+  --blue-color: #00f0ff;
+}
+
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  outline: none;
+  background-color: var(--yellow-color);
+  color: var(--black-color);
+  cursor: pointer;
+  padding: 20px 25px;
+  position: relative;
+  font-family: Tomorrow, sans-serif;
+  font-size: .85rem;
+  text-transform: uppercase;
+}
+`
