25ffe406e2b19c7ad5d3cb3cd90bfbf050096412
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-01 21:37:18 +0530

    Update Button.js

--- client/components/CyberPunkButton/Button.js
+++ client/components/CyberPunkButton/Button.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { ParentButton, ButtonContent, ButtonLabel } from './Button.elements'
+import { ParentButton, ButtonContent, ButtonLabel, GlitchFx } from './Button.elements'
 
 function Button() {
     return (
         <ParentButton>
             <ButtonContent>2077 Text_</ButtonContent>
             <ButtonLabel>r25</ButtonLabel>
+            <GlitchFx></GlitchFx>
         </ParentButton>
     )
 }
