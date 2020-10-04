dd1eee266c06c738a3872dcf818d0ca4ca00fdf8
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-10-04 15:27:02 +0530

    Update Navbar.elements.js

--- client/components/Navbar/Navbar.elements.js
+++ client/components/Navbar/Navbar.elements.js
@@ -1,28 +1,33 @@
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: rgb(0 208 255);
-height: 80px;
-display: flex;
-align-self: center;
-position: sticky;
-top: 0;
-z-index: 999;
-font-size: 1.2rem;
-padding: 1rem;
+    background: rgb(0 208 255);
+    height: 70px;
+    display: flex;
+    align-self: center;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+    font-size: 1.2rem;
+    padding: 1rem;
+    justify-content: space-between;
+     
+    &:hover {
+        color: red;
+    }
 
-&:hover {
-    color: red;
-    align-items: center;
-}
-
-@media (max-width: 768px){
-    color: white;
-}
+    @media (max-width: 768px){
+        color: white;
+    }
 `
 
 export const NavContainer = styled.div`
-display: flex;
-justify-content: space-between;
-height: 80px;
+    display: flex;
+    justify-content: space-between;
+    height: 70px;
+`
+
+export const NavLink = styled.a`
+    color: wheat;
+    margin-left: 1rem;
 `
